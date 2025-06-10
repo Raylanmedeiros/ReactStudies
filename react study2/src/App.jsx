a79d61f35251acb78bcd4c9f4b51a5599fca38c1
Raylan Medeiros <raylanmedeiros.rm@gmail.com>
2025-06-10 16:22:46 -0300

    new studies

---
+++ react study2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+// Define o componente App
+// Este componente é o ponto de entrada da aplicação React
+// Ele renderiza um título e uma descrição simples
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Welcome to My React App</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+}
+ 
+// Exporta o componente App para ser usado em outros arquivos
+export default App;
